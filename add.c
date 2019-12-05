40260e965a806b37239b8e358a1d281bdeb9af19
subhajit8085 <subhajit28061997@gmail.com>
2019-12-05 19:40:20 +0000

    add1.c

--- add.c
+++ add.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
- 
+
 int main() {
-   int a, b, sum;
- 
+   int a, b, sum,sub;
+
    printf("\nEnter two no: ");
    scanf("%d %d", &a, &b);
- 
+
    sum = a + b;
- 
+   sub = a-b;
+
+
    printf("Sum : %d", sum);
- 
+    printf("Sub : %d", sub);
+
    return(0);
 }
